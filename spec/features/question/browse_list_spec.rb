6dab516c405599a091986f924786f762a59a5636
Snblack <koracell@yandex.ru>
2020-08-26 14:04:41 +0300

    browse_question_spec

--- spec/features/question/browse_list_spec.rb
+++ spec/features/question/browse_list_spec.rb
@@ -6,7 +6,7 @@ feature 'any user can browse list of questions' do
     create_list(:question, 3)
 
     visit root_path
-    
+
     expect(page).to have_content 'List of questions'
     expect(page).to have_content 'MyString'
     expect(page).to have_content 'MyText'
---
+++ spec/features/question/browse_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature 'user can browse question and answers of question' do
+
+  scenario 'browses question' do
+    question = create(:question)
+    question.answers.create(body: 'test')
+
+    visit question_path(question)
+    save_and_open_page
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content question.answers[0].body
+  end
+
+end
