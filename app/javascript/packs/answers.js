cb33058e572b2c56af267226fbf7ab1e5e1a4295
Snblack <koracell@yandex.ru>
2020-10-11 19:57:07 +0300

    v12_js_hidden_answer

--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -6,12 +6,26 @@ $(document).on('turbolinks:load', function(){
     $('form#edit-answer-' + answerId).removeClass('hidden');
   });
 
-  $('p.vote_answer')
+  $('.vote_answer_up')
       .on('ajax:success', function(e) {
         var answer = e.detail[0]
-        $('.answer-errors').html('');
         $('p.rating_answer_' + answer.id).html('<p>' + answer.rating + '</p>')
+        $('.vote_answer_up.id-' + answer.id).addClass('hidden')
+        $('.vote_answer_down.id-' + answer.id).removeClass('hidden')
+      })
+      .on('ajax:error', function (e) {
+        var errors = e.detail[0];
+        $.each(errors, function(index, value) {
+          $('.answer-errors').html('<p>' + value + '</p>');
+      })
+  });
 
+  $('.vote_answer_down')
+      .on('ajax:success', function(e) {
+        var answer = e.detail[0]
+        $('p.rating_answer_' + answer.id).html('<p>' + answer.rating + '</p>')
+        $('.vote_answer_down.id-' + answer.id).addClass('hidden')
+        $('.vote_answer_up.id-' + answer.id).removeClass('hidden')
       })
       .on('ajax:error', function (e) {
         var errors = e.detail[0];
