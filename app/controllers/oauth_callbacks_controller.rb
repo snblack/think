2c1e6e7112512becf80d716c1b5d230f6200a99b
Snblack <koracell@yandex.ru>
2020-10-30 17:42:33 +0300

    correct_dry

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,23 +1,23 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
     def github
-      @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-      if @user&.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
-      else
-        redirect_to root_path, alert: 'Something went wrong'
-      end
+      autorize('GitHub')
     end
 
     def facebook
+      autorize('Facebook')
+    end
+
+    private
+
+    def autorize(kind)
       @user = User.find_for_oauth(request.env['omniauth.auth'])
 
       if @user&.persisted?
         sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+        set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
       else
         redirect_to root_path, alert: 'Something went wrong'
       end
     end
+
 end
