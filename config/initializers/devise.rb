cdbc54a31ab470beff3595fe73db003bd7d8983e
Snblack <koracell@yandex.ru>
2020-11-28 21:14:29 +0300

    correct_credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,9 +272,6 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  puts "Rails.env.to_sym: #{Rails.env.to_sym}"
-  puts "Rails.application.credentials: #{Rails.application.credentials}"
-  puts "Rails.application.credentials[Rails.env.to_sym][:github][:app_id]: #{Rails.application.credentials[Rails.env.to_sym][:github][:app_id]}"
 
   config.omniauth :github,
                   Rails.application.credentials[Rails.env.to_sym][:github][:app_id],
